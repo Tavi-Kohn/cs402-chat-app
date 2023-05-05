9e5f1e813f820ee1df60e21ccdf8297b152ee3f5
Tavi Kohn <tavikohn@u.boisestate.edu>
2023-05-04 21:07:31 -0600

    Switch sign in to button

--- Screens/Signup.js
+++ Screens/Signup.js
@@ -1,69 +1,8 @@
-// import React,{useState} from "react";
-// import { createUserWithEmailAndPassword } from "firebase/auth";
-// import {auth} from "../config/firebase"
-// import { Alert, TouchableOpacity, View } from "react-native";
-// import { SafeAreaView } from "react-native-safe-area-context";
-// import { TextInput } from "react-native-gesture-handler";
-// import { Text } from "react-native";
-
-
-// export default function Signup({navigation})
-// {
-//     const [email,setEmail]=useState("");
-//     const [password,setPassword]=useState("");
-
-//     const onHandleSignup=()=>
-//     {
-//         if(email !=="" && password !=="")
-//         {
-//             createUserWithEmailAndPassword(auth,email,password)
-//             .then(()=>console.log("Signup Check!"))
-//             .catch((err)=>Alert.alert("Login error",err.message))
-//         }
-//     };
-//     return(
-//         <View>
-//             <View>
-//                 <SafeAreaView>
-//                     <Text>Signup</Text>
-//                     <TextInput
-//                         placeholder="Enter Email"
-//                         keyboardType="email-address"
-//                         textContentType="emailAddress"
-//                         autoFocus={true}
-//                         value={email}
-//                         onChangeText={(text)=>setEmail(text)}
-//                     />
-
-//                     <TextInput
-//                         placeholder="Enter Password"
-//                         autoCorrect={false}
-//                         secureTextEntry={true}
-//                         textContentType="password"
-//                         value={password}
-//                         onChangeText={(text)=>setPassword(text)}
-//                     />
-//                     <TouchableOpacity onPress={onHandleSignup}>
-//                         <Text>Signup</Text>
-//                     </TouchableOpacity>
-//                     <View>
-//                         <Text style={{color:'gray',fontWeight:'600',fontSize:14}}>Have an account?</Text>
-//                         <TouchableOpacity onPress={()=>navigation.navigate("Login")}>
-//                         <Text style={{color:'orange',fontWeight:'600',fontSize:14}}>Log In</Text>
-//                         </TouchableOpacity>
-
-//                     </View>
-//                 </SafeAreaView>
-//             </View>
-//         </View>
-//     )
-// }
-
 import React,{useState,useLayoutEffect,useCallback,} from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { collection,addDoc,orderBy,query,onSnapshot } from "firebase/firestore";
 import {auth,database} from '../config/firebase'
-import { Alert, TouchableOpacity, View } from "react-native";
+import { Alert, TouchableOpacity, View, Button } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { TextInput } from "react-native-gesture-handler";
 import { Text } from "react-native";
@@ -153,9 +92,7 @@ export default function Login({navigation})
                         value={password}
                         onChangeText={(text)=>setPassword(text)}
                     />
-                    <TouchableOpacity onPress={onHandleLogin}>
-                        <Text>Login</Text>
-                    </TouchableOpacity>
+                    <Button title="Login" onPress={onHandleLogin}></Button>
                     <View>
                         <Text style={{color:'gray',fontWeight:'600',fontSize:14}}>Don't have an account?</Text>
                         <TouchableOpacity onPress={()=>navigation.navigate("Signup")}>
