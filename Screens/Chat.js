72fe4081e140c6b0d94ca68675e4427cad735f4b
Tavi Kohn <tavikohn@u.boisestate.edu>
2023-05-04 21:01:41 -0600

    Store user info with messages

--- Screens/Chat.js
+++ Screens/Chat.js
@@ -12,7 +12,6 @@ import { useRoute } from '@react-navigation/native';
 export default function Chat({propKey }){
     const[message,setMessage]=useState([]);
     const navigation=useNavigation();
-    const[user,setUser]=useState('robbies var');
     const route = useRoute();
     const myParam = route.params.myParam;
     console.log(myParam)
@@ -53,7 +52,6 @@ export default function Chat({propKey }){
         //message.user = 'Robbie'
         //message[0].user = {name: user};
         //message[0].user1 = 'Robbie';
-        console.log(message)
         setMessage(previousMessage=>GiftedChat.append(previousMessage,message));
         const {_id,createdAt,text,user}=message[0];
         addDoc(collection(database,myParam),{
@@ -91,7 +89,7 @@ export default function Chat({propKey }){
             //    _id:2,
             //    user:user
            // }}
-           user={user}
+            user={{_id: auth.currentUser.uid, name: auth.currentUser.email}}
             onSend={message=>OnSend(message)}
             
             //renderBubble={}
